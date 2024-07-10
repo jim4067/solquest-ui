f4b62040ab41547df3ecd834675477387c909818
Jimii <jimmyimpulse2@gmail.com>
2024-07-10 18:47:27 +0300

    feat: stake assets

--- app/nft/components.tsx
+++ app/nft/components.tsx
@@ -9,9 +9,15 @@ import { isSome, publicKey as UmiPublicKey } from "@metaplex-foundation/umi";
 
 import { css } from "@/styled-system/css";
 import { flex } from "@/styled-system/patterns";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+	PublicKey,
+	Transaction,
+	TransactionInstruction,
+} from "@solana/web3.js";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
+import { useSolquestSDK } from "../hooks/sdk";
 import { useUmi } from "../hooks/umi";
 import { Card } from "../ui/components/Card";
 import TabRoot, {
@@ -99,10 +105,25 @@ export function Rewards({ label, value }: RewardsProps) {
 	);
 }
 
+type SolquestNFTType = Partial<DigitalAsset & { image: string; name: string }>;
 type CardListProps = {
-	nfts: Array<Partial<DigitalAsset & { image: string; name: string }>>;
+	nfts: Array<SolquestNFTType>;
+	onClick: (nft: SolquestNFTType, id: number) => void;
 };
-function CardList({ nfts }: CardListProps) {
+function CardList({ nfts, onClick }: CardListProps) {
+	const [selectedIndices, setSelectedIndices] = useState<Array<number>>([]);
+
+	const handleClick = (nft: SolquestNFTType, index: number) => {
+		setSelectedIndices((prevSelected: any) => {
+			if (prevSelected.includes(index)) {
+				return prevSelected.filter((i: number) => i !== index);
+			} else {
+				return [...prevSelected, index];
+			}
+		});
+		onClick(nft, index);
+	};
+
 	return (
 		<section
 			className={flex({
@@ -118,23 +139,28 @@ function CardList({ nfts }: CardListProps) {
 					collection={nft?.metadata?.name ?? ""}
 					// collection={nft.metadata.collection ?? ""}
 					image={nft?.image ?? ""}
+					onClick={() => handleClick(nft, index)}
+					isSelected={selectedIndices.includes(index)}
 				/>
 			))}
 		</section>
 	);
 }
 
-let staked = new Array(6).fill({
-	title: "#1000",
-	collection: "this SOL NFT has a long name",
-	image: "https://s3-alpha-sig.figma.com/img/82e9/6d96/cedb304aa33425a560249d862e797ccf?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EBj-jpVMfxze01i8eZgieNJDhtvg9GY1WzGmEKlW8TQj5cFJAzdkuO2aIgWnRiLPZjDbHUGv8d~AIX9SFwFdL7NP6tUr41-PnXVyBZzZ-d5z4xapshTCdK8R0WAghwv2mTBLGXxWxaY-4hBSDX~SSvBKfAZJbSap8PHe0gJfXRu5tG9w4tGEYhW9fN6-uQ6SsgWhPG-XbAvTuE07sQ9~TUu66C4vp39n2orFYMigocb2gqG3iARObML1RA0KQR57g4RtAVCUQw6Z3vpbZHkmsWWlpmL4toqeAwZoQXgLH0B6AoGVP389b6YMoZvGbrYGTXHgoWGJAbjESHi8U6gX4A__",
-});
-
 export function NFTTabs() {
-	const { publicKey } = useWallet();
-	const { umiInstance } = useUmi();
+	const { publicKey, sendTransaction, signTransaction } = useWallet();
+	const { connection } = useConnection();
 
-	const [userAssets, setUserAssets] = useState<any>();
+	const { umiInstance } = useUmi();
+	const { solquestNFT } = useSolquestSDK();
+
+	const [unstakedUserAssets, setUnstakedUserAssets] = useState<any>();
+	const [stakedUserAssets, setStakedUserAssets] = useState<any[]>(() => {
+		const stored = localStorage.getItem("staked");
+		return stored ? JSON.parse(stored) : [];
+	});
+
+	const [selectedToStake, setSelectedToStake] = useState<any[]>([]);
 
 	const fetchAssets = useCallback(async () => {
 		if (!publicKey || !umiInstance) {
@@ -150,12 +176,21 @@ export function NFTTabs() {
 		const res = await Promise.all(
 			digitalAssets
 				.filter((asset) => isSome(asset.metadata.collection))
+				.filter(
+					(asset) =>
+						!new Set(
+							stakedUserAssets?.map(
+								(stakedAsset) => stakedAsset.mint.publicKey
+							)
+						).has(asset.mint.publicKey)
+				)
 				.map(async (asset) => {
 					const offChainData = (await axios.get(asset.metadata.uri))
 						.data;
 					return {
 						image: offChainData.image,
 						name: offChainData.name,
+						mint: asset.mint,
 						asset,
 						offChainData,
 					};
@@ -163,17 +198,77 @@ export function NFTTabs() {
 		);
 
 		return res;
-	}, [publicKey, umiInstance]);
+	}, [publicKey, umiInstance, stakedUserAssets]);
 
 	useEffect(() => {
 		if (!publicKey || !umiInstance) {
 			return;
 		}
 
+		// fetch unstaked user assets
 		fetchAssets()
-			.then((res) => setUserAssets(res))
+			.then((res) => setUnstakedUserAssets(res))
 			.catch(console.error);
-	}, [fetchAssets, publicKey, umiInstance]);
+	}, [fetchAssets, publicKey, umiInstance, stakedUserAssets]);
+
+	const handleStakeSelected = useCallback(async () => {
+		if (!selectedToStake || !solquestNFT || !signTransaction) {
+			return;
+		}
+		if (selectedToStake && selectedToStake?.length > 12) {
+			alert("cannot stake more than items at a time");
+			return;
+		}
+		const stakeIxs = await Promise.all(
+			selectedToStake.map(
+				async (asset): Promise<TransactionInstruction> => {
+					return await solquestNFT.stakeNFT(
+						new PublicKey(asset.mint.publicKey),
+						new PublicKey(asset.metadata.collection.key)
+					);
+				}
+			)
+		);
+		// transaction
+		const tx = new Transaction().add(...stakeIxs);
+		await sendTransaction(tx, connection);
+		// save the asset inside window storage
+		if (!stakedUserAssets) {
+			localStorage.setItem("staked", JSON.stringify(selectedToStake));
+			return;
+		}
+		localStorage.setItem(
+			"staked",
+			JSON.stringify([...selectedToStake, ...stakedUserAssets])
+		);
+	}, [
+		selectedToStake,
+		solquestNFT,
+		signTransaction,
+		sendTransaction,
+		connection,
+		stakedUserAssets,
+	]);
+
+	const handleSelection = (solqAsset: SolquestNFTType) => {
+		setSelectedToStake((prevSelected: Array<SolquestNFTType>) => {
+			const isAlreadySelected = prevSelected.some(
+				(n: SolquestNFTType) =>
+					n.mint?.publicKey.toString() ===
+					solqAsset.mint?.publicKey.toString()
+			);
+
+			if (isAlreadySelected) {
+				return prevSelected.filter(
+					(n: SolquestNFTType) =>
+						n.mint?.publicKey.toString() !==
+						solqAsset.mint?.publicKey.toString()
+				);
+			} else {
+				return [...prevSelected, solqAsset];
+			}
+		});
+	};
 
 	return (
 		<section>
@@ -187,14 +282,21 @@ export function NFTTabs() {
 						<SelectAllBtn
 							onClick={() => console.log("select all button")}
 						/>
-						<StakeBtn onClick={() => console.log("stake button")} />
+						<StakeBtn onClick={handleStakeSelected} />
 					</section>
 				</TabHeaderList>
 				<TabContent value="Staked">
-					<CardList nfts={staked} />
+					<CardList
+						// @ts-ignore
+						nfts={stakedUserAssets}
+						onClick={() => console.log("cant stake a staked asset")}
+					/>
 				</TabContent>
 				<TabContent value="Unstaked">
-					<CardList nfts={userAssets} />
+					<CardList
+						nfts={unstakedUserAssets}
+						onClick={(nft, id) => handleSelection(nft)}
+					/>
 				</TabContent>
 			</TabRoot>
 		</section>
